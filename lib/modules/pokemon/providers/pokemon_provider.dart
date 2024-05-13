74e0a17ca1dfefecc40fd95b5a3f1597db8770ef
Nathanael Ama <nathanael.ama1@gmail.com>
2024-05-13 15:38:46 +0200

    feat(core): add pokemonList and currentPokemon providers

--- lib/modules/pokemon/providers/pokemon_provider.dart
+++ lib/modules/pokemon/providers/pokemon_provider.dart
@@ -1,4 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yapdex/core/data/models/pokemon/pokemon.dart';
+import 'package:yapdex/core/data/models/utility/common.dart';
 import 'package:yapdex/core/data/provider/dio_provider.dart';
 import 'package:yapdex/core/data/repositories/pokemon_repository.dart';
 
@@ -9,3 +11,55 @@ PokemonRepository pokemonRepository(PokemonRepositoryRef ref) =>
     PokemonRepository(
      dio: ref.watch(dioProvider),
     );
+
+@Riverpod(keepAlive: true)
+class PokemonList extends _$PokemonList {
+  List<Pokemon> _pokemonList = [];
+
+  void getPokemonList() {
+    final pokemonRepository = ref.read(pokemonRepositoryProvider);
+    state = const AsyncLoading();
+    pokemonRepository.getPokemons().then((value) {
+      _pokemonList = value;
+      state = AsyncData(_pokemonList);
+    });
+  }
+
+  @override
+  Future<List<Pokemon>> build() async {
+    return _pokemonList;
+  }
+}
+
+@Riverpod(keepAlive: true)
+class CurrentPokemon extends _$CurrentPokemon {
+  Pokemon _currentPokemon = const Pokemon(
+    id: 0,
+    name: '',
+    species: NamedAPIResource(name: '', url: ''),
+    abilities: [],
+    stats: [],
+    types: [],
+    weight: 0,
+  );
+
+  void setCurrentPokemon(Pokemon pokemon) {
+    _currentPokemon = pokemon;
+    state = pokemon;
+
+    
+  }
+
+  void setCurrentPokemonById(int id) {
+    final pokemonRepository = ref.read(pokemonRepositoryProvider);
+    pokemonRepository.getPokemonById(id: id).then((pokemon) {
+      _currentPokemon = pokemon;
+      state = _currentPokemon;
+    });
+  }
+
+  @override
+  Pokemon build() {
+    return _currentPokemon;
+  }
+}
--- lib/modules/pokemon/providers/pokemon_provider.g.dart
+++ lib/modules/pokemon/providers/pokemon_provider.g.dart
@@ -6,7 +6,7 @@ part of 'pokemon_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$pokemonRepositoryHash() => r'f6a97bbf99e9edb8f4fb45851c32bfd5ffa9f206';
+String _$pokemonRepositoryHash() => r'1720e9d29d2d2956954c26046718d851149f0cbb';
 
 /// See also [pokemonRepository].
 @ProviderFor(pokemonRepository)
@@ -21,5 +21,36 @@ final pokemonRepositoryProvider = Provider<PokemonRepository>.internal(
 );
 
 typedef PokemonRepositoryRef = ProviderRef<PokemonRepository>;
+String _$pokemonListHash() => r'649b748657198b7f614ba233b55395528a666337';
+
+/// See also [PokemonList].
+@ProviderFor(PokemonList)
+final pokemonListProvider =
+    AsyncNotifierProvider<PokemonList, List<Pokemon>>.internal(
+  PokemonList.new,
+  name: r'pokemonListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$pokemonListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PokemonList = AsyncNotifier<List<Pokemon>>;
+String _$currentPokemonHash() => r'2bceed2e5dd8acf66b14ecc4a1c7a3948c9ae0b3';
+
+/// See also [CurrentPokemon].
+@ProviderFor(CurrentPokemon)
+final currentPokemonProvider =
+    NotifierProvider<CurrentPokemon, Pokemon>.internal(
+  CurrentPokemon.new,
+  name: r'currentPokemonProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$currentPokemonHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CurrentPokemon = Notifier<Pokemon>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
