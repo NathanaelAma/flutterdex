9f4a736b74eae8ac832640d6f17b8674a0b7b750
Nathanael Ama <nathanael.ama1@gmail.com>
2024-06-08 18:20:54 +0200

    feat(core): add utils class for commonly needed methods

---
+++ lib/core/utils/utils.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+/// Converts the given [text] to uppercase and returns the modified string.
+String upperCase(String text) {
+  return text.toUpperCase();
+}
+
+/// Converts the first character of the given [pokemonName] to uppercase and returns the modified string.
+String upperCasePokemonName(String pokemonName) {
+  return pokemonName.characters.first.toUpperCase() + pokemonName.substring(1);
+}
+
+/// Formats the given [description] by replacing certain characters and returns the modified string.
+String formatDescription(String description) {
+  return description
+      .replaceAll('\f', '\n')
+      .replaceAll('\u00ad\n', '')
+      .replaceAll('\u00ad', '')
+      .replaceAll(' -\n', ' - ')
+      .replaceAll('-\n', '-')
+      .replaceAll('\n', ' ');
+}
