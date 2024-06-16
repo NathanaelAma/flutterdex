0beafe1686951aca91ec683a2cb99a2215a016ca
Nathanael Ama <nathanael.ama1@gmail.com>
2024-06-16 15:54:27 +0200

    feat: update scaffold colours and add description field for about section

--- lib/modules/pokemon/ui/widgets/about_section.dart
+++ lib/modules/pokemon/ui/widgets/about_section.dart
@@ -5,34 +5,54 @@ import 'package:yapdex/modules/common/widgets/type_icon.dart';
 import 'package:yapdex/modules/pokemon/providers/pokemon_provider.dart';
 
 class AboutSection extends StatelessWidget {
-  const AboutSection({super.key});
+  const AboutSection({super.key, required this.description});
+  final String description;
 
   @override
   Widget build(BuildContext context) {
     //TODO: figure out how to get pokemon species text without getting all the other versions
-    return Center(
-      child: Consumer(
-        builder: (context, ref, child) {
-          final String type =
-              ref.watch(currentPokemonProvider).types.first.type.name;
+    return Consumer(
+      builder: (context, ref, child) {
+        final String type =
+            ref.watch(currentPokemonProvider).types.first.type.name;
 
-          return Column(
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(48),
+              topRight: Radius.circular(48),
+            ),
+            color: Colors.white,
+          ),
+          child: Column(
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TypeIcon(type: type, iconOnly: false),
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in elit ipsum. Nulla facilisi. Nullam vel iaculis lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam a auctor nulla. Nullam porta libero lectus, eu porta dui auctor non. In mi libero, porttitor ut luctus.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16)),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                          description
+                              .replaceAll('\f', '\n')
+                              .replaceAll('\u00ad\n', '')
+                              .replaceAll('\u00ad', '')
+                              .replaceAll(' -\n', ' - ')
+                              .replaceAll('-\n', '-')
+                              .replaceAll('\n', ' '),
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(fontSize: 16)),
+                    ),
+                  ],
+                ),
               ),
             ],
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
